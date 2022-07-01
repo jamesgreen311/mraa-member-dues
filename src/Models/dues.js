440ca46dcb510f1de032cc7cb98279d6d4894247
james green <jamesgreen.311@gmail.com>
2022-07-01 11:50:28 -0400

    Initial Commit

---
+++ src/Models/dues.js
@@ -0,0 +1,6 @@
+const duesDB = conn(MEMBER_DUES_ID)
+const MD_TABLES = {
+    "payments" : "Payments",
+    "history" : "Payment History",
+    "settings" : "Settings"
+}
---
+++ src/Models/members.js
@@ -0,0 +1,5 @@
+const membersDB = conn(MEMBERSHIP_ID)
+const M_TABLES = {
+    "directory" : "Member Directory",
+    "settings" : "Configuration"
+}
